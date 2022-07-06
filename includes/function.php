605167060d81c4f38fe5d86f94c9d3218a48ecb8
Quentin Cliquot <quentincliquot@gmail.com>
2022-07-06 09:52:14 +0200

    jour 1

---
+++ includes/function.php
@@ -0,0 +1,34 @@
+<?php
+
+function debug($X)
+{
+    echo '<pre style="height:200px;overflow-y: scroll;font-size:.5rem;padding: .6rem; font-family: Consolas, Monospace;background-color: #000;color:#fff;">';
+    print_r($X);
+    echo '</pre>';
+}
+
+function validText($er, $data, $key, $min, $max)
+{
+    if (!empty($data)) {
+        if (mb_strlen($data) < $min) {
+            $er[$key] = 'min ' . $min . ' caractères';
+        } elseif (mb_strlen($data) >= $max) {
+            $er[$key] = 'max ' . $max . ' caractères';
+        }
+    } else {
+        $er[$key] = 'Veuillez renseigner ce champ';
+    }
+    return $er;
+}
+
+function validEmail($er, $data, $key)
+{
+    if (!empty($data)) {
+        if (!filter_var($data, FILTER_VALIDATE_EMAIL)) {
+            $er[$key] = 'Veuillez renseigner un email valide';
+        }
+    } else {
+        $er[$key] = 'Veuillez renseigner un email';
+    }
+    return $er;
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Formulaire-M</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom">
+
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom">
+
+        <label for="nom">Nom : </label>
+        <input type="email" name="nom" id="nom">
+
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom">
+    </form>
+</body>
+</html>
